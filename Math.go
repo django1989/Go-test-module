4b6557cc610be07925c08e950dbc91d49d9a9abb
root <f.jalalizadeh@outook.com>
2024-10-05 22:36:41 +0330

    add new module

---
+++ Math.go
@@ -0,0 +1,10 @@
+package Math
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func subtract(a, b int) int {
+	return a - b
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/django1989/Go-test-module
+
+go 1.22.5
